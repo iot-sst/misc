4f3d52036a14d1daa68cb0e032ea24462cff3a43
iot-sst <43617120+iot-sst@users.noreply.github.com>
2019-09-09 12:34:30 +0100

    Add files via upload

--- ssh-side-a.sh
+++ ssh-side-a.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S sshpass glibc
+sudo pacman --noconfirm -S sshpass glibc
 wget https://iot-sst.github.io/misc/ilstunnelsshA.service
 sudo mv ./ilstunnelsshA.service /etc/systemd/system/ilstunnelssh.service
 sudo systemctl start ilstunnelssh
--- ssh-side-b.sh
+++ ssh-side-b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S sshpass glibc
+sudo pacman --noconfirm -S sshpass glibc
 wget https://iot-sst.github.io/misc/ilstunnelsshB.service
 sudo mv ./ilstunnelsshB.service /etc/systemd/system/ilstunnelssh.service
 sudo systemctl start ilstunnelssh
