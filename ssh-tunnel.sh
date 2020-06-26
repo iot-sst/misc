48b81da8d03bc4c7221e701e0d0551c64f8d29a3
psampaio <psampaio.isel@gmail.com>
2020-06-26 09:22:33 +0100

    update

--- ssh-tunnel.sh
+++ ssh-tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo pip3 install psutil
 sudo pacman --noconfirm -Syy sshpass glibc
+sudo pip3 install psutil
 wget https://iot-sst.github.io/misc/supervisor.py
 wget https://iot-sst.github.io/misc/ilstunnelssh.service
 sudo mv ./ilstunnelssh.service /etc/systemd/system/ilstunnelssh.service
